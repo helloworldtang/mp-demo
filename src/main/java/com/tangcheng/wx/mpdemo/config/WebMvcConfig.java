1231fa5b16fadbfd0dd6053b6f26c1b207d49b56
唐成 <tangcheng@hujiang.com>
2017-10-26 10:09:43 +0800

    折衷方案。将StringHttpMessageConverter放在FastJsonHttpMessageConverter前面 返回的字符串会使用StringHttpMessageConverter转换

--- src/main/java/com/tangcheng/wx/mpdemo/config/WebMvcConfig.java
+++ src/main/java/com/tangcheng/wx/mpdemo/config/WebMvcConfig.java
@@ -6,7 +6,9 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Date;
@@ -19,6 +21,14 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        // See SPR-7316,for weixin token verify.fastjson will convert string 123 to "123"
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+        stringHttpMessageConverter.setWriteAcceptCharset(false);
+        //增加两个优先处理的转换类型.
+        converters.add(new ByteArrayHttpMessageConverter());
+        converters.add(stringHttpMessageConverter);
+
         FastJsonHttpMessageConverter httpMessageConverter = new FastJsonHttpMessageConverter();
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(SerializerFeature.QuoteFieldNames,
